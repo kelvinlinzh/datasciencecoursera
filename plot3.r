c601aecf5750a90e67625b6ec698d8e063ca0a9e
Kelvin Lin <klin3@e.ntu.edu.sg>
2015-04-12 17:17:05 +0800

    Merge remote-tracking branch 'origin/master'
    Exploratory Data Analysis
    
    Week 1 assignment

---
+++ plot3.r
@@ -0,0 +1,20 @@
+windows()
+## Read full dataset
+data_full <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+## Convert Date; make sure its capital Y for YYYY
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+## Subset dates 2007-02-01 to 2007-02-02
+data <- subset(data_full, Date >= "2007-02-01" & Date <= "2007-02-02")
+## Combine date and time to get continuous line
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+## Format Data
+data$Global_active_power <- as.numeric(data$Global_active_power)
+## Plot 3
+plot(data$Sub_metering_1~data$Datetime, type="l", xlab="", ylab="Energy sub metering")
+lines(data$Sub_metering_2~data$Datetime,col='Red')
+lines(data$Sub_metering_3~data$Datetime,col='Blue')
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+## Save as PNG
+dev.copy(png, file = "plot3.png", height = 480, width = 480)
+dev.off()
